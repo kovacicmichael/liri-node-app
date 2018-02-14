b6a8891b6be74a184dcd7f00f14a73f2f2330e15
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-02-13 21:38:06 -0700

    all npms installed

--- liri.js
+++ liri.js
@@ -1,1 +1,21 @@
 require("dotenv").config();
+
+var apikeys = require("./keys.js")
+
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var request = require('request'); //OMDB
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+
+  
+});
